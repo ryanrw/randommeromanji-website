98f9285f3dd890a7f705c2108d6d3ade2288cbb5
Ryan-Willpower <surasak.c.work@gmail.com>
2019-06-17 02:36:36 +0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Global } from '@emotion/core'
 import globalCSS from './utils/global'
+import Header from './components/header'
 
 const App: React.FC = () => (
   <div>
+    <Header />
     <div>Hello World!</div>
     <Global styles={globalCSS} />
   </div>
