cef5ac4a4be68dbe5ae6d7466fc189cf0f28c293
Ryan-Willpower <surasak.c.work@gmail.com>
2019-06-21 08:19:49 +0700

    add folder entry

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+import useInputData from './inputData'
+import useRandomWord from './randomWord'
+import randomNumber from './randomNumber'
+import useModalHandler from './showModalHandler'
+import useSubmitEvent from './submitData'
+import globalCSS from './global'
+
+export {
+  useInputData,
+  useRandomWord,
+  randomNumber,
+  useModalHandler,
+  useSubmitEvent,
+  globalCSS,
+}
